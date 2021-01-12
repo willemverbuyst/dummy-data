9d69d27db483322b8f898032e1fac6bb43276ada
Willem Verbuyst <willemverbuyst@gmail.com>
2021-01-12 17:24:58 +0100

    Shuffle array with questions

--- school_tests.sh
+++ school_tests.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
 # Create a file
-
 touch sb_dummy_school_tests.txt
+
+shuffle() {
+  local i tmp size max rand
+
+  # Get the size of the array
+  size=${#array[*]}
+
+  # $RANDOM % (i+1) is biased because of the limited range of $RANDOM
+  # Compensate by using a range which is a multiple of the array size.
+  max=$(( 32768 / size * size ))
+
+  for ((i=size-1; i>0; i--)); do
+    while (( (rand=$RANDOM) >= max )); do :; done
+    rand=$(( rand % (i+1) ))
+    tmp=${array[i]} array[i]=${array[rand]} array[rand]=$tmp
+  done
+}
+
+# Create an array with numbers 1 to 5
+array=( $(seq 5) )
 
 # Loop over ssubjects (3)
 # Loop over students (3)
@@ -16,18 +35,20 @@ do
   do
     for k in {1..10}
     do
+      shuffle
       echo "{
-        question1: 1,
-        question2: 2,
-        question3: 3,
-        answer1: $(($RANDOM%2)),
-        answer2: $(($RANDOM%2)),
-        answer3: $(($RANDOM%2)),
-        createdAt: new Date(new Date().setDate(new Date().getDate() - $k)),
-        updatedAt: new Date(new Date().setDate(new Date().getDate() - $k)),
-        subjectId: $j,
-        studentId: $i,
+  question1: ${array[0]}, 
+  question2: ${array[1]}, 
+  question3: ${array[2]}, 
+  answer1: $(($RANDOM%2)),
+  answer2: $(($RANDOM%2)),
+  answer3: $(($RANDOM%2)),
+  createdAt: new Date(new Date().setDate(new Date().getDate() - $k)),
+  updatedAt: new Date(new Date().setDate(new Date().getDate() - $k)),
+  subjectId: $j,
+  studentId: $i,
 }," >> sb_dummy_school_tests.txt
     done
   done
 done
+
