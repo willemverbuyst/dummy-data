3bc08d5d5e8159f3a27767c66fe4cd40e3d30cc5
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-25 09:49:21 +0200

    Move data into js file

--- logData.sh
+++ logData.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
 # Create a file
-touch logData.txt
+touch logData.js
 
 # Delete previous data
-> logData.txt
+> logData.js
 
-#! /bin/bash 
-arr_csv=() 
-while IFS= read -r line 
+# arr_csv=() 
+while IFS="|" read -r rec_column1 rec_column2 rec_column3 rec_column4 rec_column5 rec_column6
 do
-  arr_csv+=("$line")
+  if [[ $rec_column3 == "home" ]]
+  then
+    arr_csv+=("{
+                date: '$rec_column2', 
+                statusOfDay: '$rec_column3',
+                weekNumber: $rec_column1
+              }")
+  else 
+    arr_csv+=("{
+                date: '$rec_column2', 
+                durationTrip: '$rec_column6', 
+                meansOfTransport: '$rec_column3', 
+                routeTripFrom: '$rec_column4',
+                routeTripTo: '$rec_column4',
+                statusOfDay: '$rec_column3',
+                weekNumber: $rec_column1
+              }")
+  fi
 done < TEST_csvLog.txt
+
+echo "const logData = [" >> logData.js
 
 # echo "Displaying the contents of array mapped from csv file:"
 index=0
 for record in "${arr_csv[@]}"
 do
-    echo "Record at index ${index}: $record"
+    echo "$record", >> logData.js
 	((index++))
 done
+
+echo "]" >> logData.js
