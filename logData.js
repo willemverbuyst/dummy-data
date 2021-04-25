0279880b9200193ace2c18a37ebeb3a1ed917595
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-25 10:12:52 +0200

    Create split functions

--- logData.js
+++ logData.js
@@ -1,124 +1,120 @@
 const logData = [
-  {
-    date: '31-03-21',
-    durationTrip: '0:35',
-    meansOfTransport: 'car',
-    routeTripFrom: 'Terneuzen - Vlissingen',
-    routeTripTo: 'Terneuzen - Vlissingen',
-    statusOfDay: 'car',
-    weekNumber: 13,
+{
+  date: '31-03-21', 
+  durationTrip: '0:35', 
+  meansOfTransport: 'car', 
+  routeTripFrom: 'Terneuzen',
+  routeTripTo: 'Vlissingen',
+  statusOfDay: 'working at the office',
+  weekNumber: 13
   },
-  {
-    date: '31-03-21',
-    durationTrip: '0:35',
-    meansOfTransport: 'car',
-    routeTripFrom: 'Vlissingen - Terneuzen',
-    routeTripTo: 'Vlissingen - Terneuzen',
-    statusOfDay: 'car',
-    weekNumber: 13,
+{
+  date: '31-03-21', 
+  durationTrip: '0:35', 
+  meansOfTransport: 'car', 
+  routeTripFrom: 'Vlissingen',
+  routeTripTo: 'Terneuzen',
+  statusOfDay: 'working at the office',
+  weekNumber: 13
   },
-  {
-    date: '01-04-21',
-    durationTrip: '0:35',
-    meansOfTransport: 'car',
-    routeTripFrom: 'Terneuzen - Vlissingen',
-    routeTripTo: 'Terneuzen - Vlissingen',
-    statusOfDay: 'car',
-    weekNumber: 13,
+{
+  date: '01-04-21', 
+  durationTrip: '0:35', 
+  meansOfTransport: 'car', 
+  routeTripFrom: 'Terneuzen',
+  routeTripTo: 'Vlissingen',
+  statusOfDay: 'working at the office',
+  weekNumber: 13
   },
-  {
-    date: '01-04-21',
-    durationTrip: '1:45',
-    meansOfTransport: 'car',
-    routeTripFrom: 'Vlissingen - Tilburg',
-    routeTripTo: 'Vlissingen - Tilburg',
-    statusOfDay: 'car',
-    weekNumber: 13,
+{
+  date: '01-04-21', 
+  durationTrip: '1:45', 
+  meansOfTransport: 'car', 
+  routeTripFrom: 'Vlissingen',
+  routeTripTo: 'Tilburg',
+  statusOfDay: 'working at the office',
+  weekNumber: 13
   },
-  {
-    date: '02-04-21',
-    durationTrip: '2:30',
-    meansOfTransport: 'public',
-    routeTripFrom: 'Tilburg - Vlissingen',
-    routeTripTo: 'Tilburg - Vlissingen',
-    statusOfDay: 'public',
-    weekNumber: 13,
+{
+  date: '02-04-21', 
+  durationTrip: '2:30', 
+  meansOfTransport: 'public', 
+  routeTripFrom: 'Tilburg',
+  routeTripTo: 'Vlissingen',
+  statusOfDay: 'working at the office',
+  weekNumber: 13
   },
-  {
-    date: '02-04-21',
-    durationTrip: '2:35',
-    meansOfTransport: 'public',
-    routeTripFrom: 'Vlissingen - Tilburg',
-    routeTripTo: 'Vlissingen - Tilburg',
-    statusOfDay: 'public',
-    weekNumber: 13,
+{
+  date: '02-04-21', 
+  durationTrip: '2:35', 
+  meansOfTransport: 'public', 
+  routeTripFrom: 'Vlissingen',
+  routeTripTo: 'Tilburg',
+  statusOfDay: 'working at the office',
+  weekNumber: 13
   },
-  {
-    date: '05-04-21',
-    durationTrip: '0:00',
-    meansOfTransport: 'day off',
-    routeTripFrom: '',
-    routeTripTo: '',
-    statusOfDay: 'day off',
-    weekNumber: 14,
+{
+  date: '05-04-21', 
+  statusOfDay: 'day off',
+  weekNumber: 14
   },
-  {
-    date: '06-04-21',
-    durationTrip: '2:30',
-    meansOfTransport: 'public',
-    routeTripFrom: 'Tilburg - Vlissingen',
-    routeTripTo: 'Tilburg - Vlissingen',
-    statusOfDay: 'public',
-    weekNumber: 14,
+{
+  date: '06-04-21', 
+  durationTrip: '2:30', 
+  meansOfTransport: 'public', 
+  routeTripFrom: 'Tilburg',
+  routeTripTo: 'Vlissingen',
+  statusOfDay: 'working at the office',
+  weekNumber: 14
   },
-  {
-    date: '06-04-21',
-    durationTrip: '2:35',
-    meansOfTransport: 'public',
-    routeTripFrom: 'Vlissingen - Tilburg',
-    routeTripTo: 'Vlissingen - Tilburg',
-    statusOfDay: 'public',
-    weekNumber: 14,
+{
+  date: '06-04-21', 
+  durationTrip: '2:35', 
+  meansOfTransport: 'public', 
+  routeTripFrom: 'Vlissingen',
+  routeTripTo: 'Tilburg',
+  statusOfDay: 'working at the office',
+  weekNumber: 14
   },
-  {
-    date: '07-04-21',
-    durationTrip: '1:35',
-    meansOfTransport: 'car',
-    routeTripFrom: 'Tilburg - Vlissingen',
-    routeTripTo: 'Tilburg - Vlissingen',
-    statusOfDay: 'car',
-    weekNumber: 14,
+{
+  date: '07-04-21', 
+  durationTrip: '1:35', 
+  meansOfTransport: 'car', 
+  routeTripFrom: 'Tilburg',
+  routeTripTo: 'Vlissingen',
+  statusOfDay: 'working at the office',
+  weekNumber: 14
   },
-  {
-    date: '07-04-21',
-    durationTrip: '0:35',
-    meansOfTransport: 'car',
-    routeTripFrom: 'Vlissingen - Terneuzen',
-    routeTripTo: 'Vlissingen - Terneuzen',
-    statusOfDay: 'car',
-    weekNumber: 14,
+{
+  date: '07-04-21', 
+  durationTrip: '0:35', 
+  meansOfTransport: 'car', 
+  routeTripFrom: 'Vlissingen',
+  routeTripTo: 'Terneuzen',
+  statusOfDay: 'working at the office',
+  weekNumber: 14
   },
-  {
-    date: '08-04-21',
-    durationTrip: '0:35',
-    meansOfTransport: 'car',
-    routeTripFrom: 'Terneuzen - Vlissingen',
-    routeTripTo: 'Terneuzen - Vlissingen',
-    statusOfDay: 'car',
-    weekNumber: 14,
+{
+  date: '08-04-21', 
+  durationTrip: '0:35', 
+  meansOfTransport: 'car', 
+  routeTripFrom: 'Terneuzen',
+  routeTripTo: 'Vlissingen',
+  statusOfDay: 'working at the office',
+  weekNumber: 14
   },
-  {
-    date: '08-04-21',
-    durationTrip: '0:35',
-    meansOfTransport: 'car',
-    routeTripFrom: 'Vlissingen - Terneuzen',
-    routeTripTo: 'Vlissingen - Terneuzen',
-    statusOfDay: 'car',
-    weekNumber: 14,
+{
+  date: '08-04-21', 
+  durationTrip: '0:35', 
+  meansOfTransport: 'car', 
+  routeTripFrom: 'Vlissingen',
+  routeTripTo: 'Terneuzen',
+  statusOfDay: 'working at the office',
+  weekNumber: 14
   },
-  {
-    date: 09 - 04 - 21,
-    statusOfDay: 'home',
-    weekNumber: 14,
+{
+  date: '09-04-21', 
+  statusOfDay: 'home',
+  weekNumber: 14
   },
-];
+]
--- logData.sh
+++ logData.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
 # Create a file
-touch logData.js
+# touch logData.js
 
 # Delete previous data
 > logData.js
 
+
+function splitStringFrom () {
+  echo $1
+}
+
+function splitStringTo () {
+  echo $3
+}
+
 # arr_csv=() 
 while IFS="|" read -r rec_column1 rec_column2 rec_column3 rec_column4 rec_column5 rec_column6
 do
-  if [[ $rec_column3 == "home" ]]
+  if [[ $rec_column3 == "home" ]] || [[ $rec_column3 == "day off" ]]
   then
     arr_csv+=("{
-                date: '$rec_column2', 
-                statusOfDay: '$rec_column3',
-                weekNumber: $rec_column1
-              }")
+  date: '$rec_column2', 
+  statusOfDay: '$rec_column3',
+  weekNumber: $rec_column1
+  }")
   else 
     arr_csv+=("{
-                date: '$rec_column2', 
-                durationTrip: '$rec_column6', 
-                meansOfTransport: '$rec_column3', 
-                routeTripFrom: '$rec_column4',
-                routeTripTo: '$rec_column4',
-                statusOfDay: '$rec_column3',
-                weekNumber: $rec_column1
-              }")
+  date: '$rec_column2', 
+  durationTrip: '$rec_column6', 
+  meansOfTransport: '$rec_column3', 
+  routeTripFrom: '$(splitStringFrom $rec_column4)',
+  routeTripTo: '$(splitStringTo $rec_column4)',
+  statusOfDay: 'working at the office',
+  weekNumber: $rec_column1
+  }")
   fi
 done < TEST_csvLog.txt
 
@@ -40,3 +49,4 @@ do
 done
 
 echo "]" >> logData.js
+
